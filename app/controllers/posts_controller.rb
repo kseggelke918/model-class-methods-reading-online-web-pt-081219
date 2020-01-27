7848e6bf4dc7de204ca5db4b304d1ccab88386d1
Katie Seggelke <katie.seggelke@gmail.com>
2020-01-27 16:43:16 +0000

    Automatically backed up by Learn

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -4,7 +4,7 @@ class PostsController < ApplicationController
     @authors = Author.all 
     
     if !params[:author].blank?
-      @posts = Post.by_author(author: params[:author])
+      @posts = Post.by_author(params[:author])
     elsif !params[:date].blank?
       if params[:date] == "Today"
         @posts = Post.from_today 
